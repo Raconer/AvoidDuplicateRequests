3498de5f356eb2151bad13feffd5444be1afaa68
Raconer <cha11enge@hanmail.net>
2022-12-13 21:29:58 +0900

    first commit

---
+++ src/main/java/com/duplicate/requests/avoid/AvoidApplication.java
@@ -0,0 +1,13 @@
+package com.duplicate.requests.avoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duplicate/requests/avoid/AvoidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duplicate.requests.avoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
